af95cc2a58106b4c87cc55febc958ba284140850
TPGarcia9090 <tpgarcia234@gmail.com>
2024-09-17 16:32:26 -0300

    Add files via upload

---
+++ Prova.js
@@ -0,0 +1,11 @@
+const n1 = 8
+const n2 = 7.2
+const n3 = 9.1
+
+const p1 = 2
+const p2 = 5
+const p3 = 3
+
+const media = (n1 * p1 + n2 * p2 + n3 * p3) / (p1 + p2 + p3)
+
+console.log('A média ponderada é: ' + media)
